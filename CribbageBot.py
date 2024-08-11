718485bc9a2d952146a6c62b792c8afe9854d643
NightFoxx254 <153968273+NightFoxx254@users.noreply.github.com>
2024-08-10 18:22:42 -0700

    Update CribbageBot.py

--- CribbageBot.py
+++ CribbageBot.py
@@ -1,4 +1,5 @@
 import random
+from itertools import combinations
 
 def findingPoints(points, hand):
     # Checking for pairs
@@ -12,19 +13,14 @@ def findingPoints(points, hand):
                 points += 2
 
     # Checking for fifteens in three cards
-    for i in range(len(hand)):
-        for j in range(i + 1, len(hand)):
-            for k in range(j + 1, len(hand)):
-                if hand[i] + hand[j] + hand[k] == 15:
-                    points += 2
+    for combo in combinations(hand, 3):
+        if sum(combo) == 15:
+            points += 2
 
     # Checking for fifteens in four cards
-    for i in range(len(hand)):
-        for j in range(i + 1, len(hand)):
-            for k in range(j + 1, len(hand)):
-                for l in range(k + 1, len(hand)):
-                    if hand[i] + hand[j] + hand[k] + hand[l] == 15:
-                        points += 2
+    for combo in combinations(hand, 4):
+        if sum(combo) == 15:
+            points += 2
 
     # Checking for fifteens in all the cards
     if len(hand) == 5 and sum(hand) == 15:
@@ -33,8 +29,6 @@ def findingPoints(points, hand):
     # Checking for runs
     hand.sort()
     run_length = 1
-    points += 0  # Initialize points for runs
-
     for i in range(1, len(hand)):
         if hand[i] == hand[i - 1] + 1:
             run_length += 1
@@ -57,40 +51,41 @@ while gameOn:
     print(" ")
     print(f"You have {playerPoints} points")
     print(f"and the computer has {points} points")
+
     # Setting up the hand
     hand = [random.randint(1, 10) for _ in range(6)]
     playerHand = [random.randint(1, 10) for _ in range(6)]
     crib = []
-    highestPoints = [0]
+    highestPoints = 0
+    handAfterCrib = []
 
-    for a in range(len(hand)):
-        for b in range(a + 1, len(hand)):
-            for c in range(b + 1, len(hand)):
-                for d in range(c + 1, len(hand)):
-                    testHand = [hand[a], hand[b], hand[c], hand[d]]
-                    currentPoints = findingPoints(0, testHand)
-                    if currentPoints > highestPoints[0]:
-                        highestPoints = [currentPoints]
-                        handAfterCrib = [a, b, c, d]
+    # Selecting cards for the crib
+    for combo in combinations(hand, 4):
+        currentPoints = findingPoints(0, list(combo))
+        if currentPoints > highestPoints:
+            highestPoints = currentPoints
+            handAfterCrib = list(combo)
 
-    # Remove cards from hand for crib
-    for index in sorted(handAfterCrib, reverse=True):
-        hand.pop(index)
+    # Remove the selected cards from hand
+    for card in handAfterCrib:
+        hand.remove(card)
 
-    crib += hand[:2]  # Add the first two cards to the crib
-    hand = hand[2:]  # Remaining cards stay in hand
+    # Add the crib cards
+    crib.extend(handAfterCrib)  # Extend the crib with the selected cards
+    hand = hand[:2]  # Keep only the first two cards in hand
 
     print("Here are your cards:")
     print(playerHand)
+
     # Da crib
     if cribChooses:
         hand += crib
         cribChooses = False
-        print("It is the computers crib")
+        print("It is the computer's crib")
     else:
         playerHand += crib
         cribChooses = True
-        print("it is your crib3")
+        print("It is your crib")
 
     for _ in range(2):
         while True:
@@ -105,29 +100,33 @@ while gameOn:
             except ValueError:
                 print("Invalid input. Please enter a number.")
 
-    beeppity = random.randint(1,10)
-    playerHand.append(beeppity)
+    beeppity = random.randint(1, 10)
     hand.append(beeppity)
-    print(f"the cut card is:{beeppity}")
+
+    print(f"The cut card is: {beeppity}")
+
     # Counting points
     newpoints = findingPoints(points, hand)
-    newplayerpoints = findingPoints(0,playerHand)
+    newplayerpoints = findingPoints(0, playerHand)
 
     print(f"Points for the computer: {newpoints}")
     print(f"Player points: {newplayerpoints}")
 
-    points +=newpoints
-    playerPoints +=newplayerpoints
+    print(hand)
+
+    points += newpoints
+    playerPoints += newplayerpoints
 
     # Logic to continue or end the game can be added here
-
-    if playerPoints >=120:
-        if points <=80:
+    if playerPoints >= 120:
+        if points <= 80:
             print("Dangggggg you skunked them")
         else:
-            print("nice job on that win")
-    elif points >=120:
-        if playerPoints <=80:
-            print("come Onnnnnnn you got skunked")
+            print("Nice job on that win")
+        gameOn = False  # End the game
+    elif points >= 120:
+        if playerPoints <= 80:
+            print("Come Onnnnnnn you got skunked")
         else:
             print("I mean you could do better next time")
+        gameOn = False  # End the game
